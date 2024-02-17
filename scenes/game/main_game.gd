63f45b428e8cb2620c205cd3625df41da642ee3d
Sylvain Grison <74255512+Machu08@users.noreply.github.com>
2024-02-17 23:12:49 +0900

    main level

---
+++ scenes/game/main_game.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+var active_enemy = null
+
+
+func _unhandled_input(event):
+	if event is InputEvent and not event.is_pressed():
+		var type
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
